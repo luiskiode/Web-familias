1615503f7f1e33581cdd6ea42d326e2acbba58d1
luiskiode <luissj002@gmail.com>
2025-08-06 16:44:42 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,20 @@
-const CACHE_NAME = "caritas-cache-v1";
+const CACHE_NAME = "caritas-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/login.html",
-  "/firebase-config.js",
-  "/supabase-config.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./login.html",
+  "./styles.css",
+  "./firebase-config.js",
+  "./supabase-config.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Instalar service worker y cachear archivos
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Servir desde caché cuando no hay internet
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
